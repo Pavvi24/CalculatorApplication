df4d3caac8017c34580f4ff6854de235812c4208
SanjayAmaravadi <sanjayamaravadisj@gmail.com>
2026-03-25 22:15:12 +0530

    Calculator Application in created

---
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -0,0 +1,19 @@
+package com.example.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+	public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+	public int add(int a, int b){
+		return a+b;
+	}
+
+	public int subtract(int a, int b){
+		return a-b;
+	}
+}
---
+++ src/test/java/com/example/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+    	public void testAdd() {
+		CalculatorApplication calc = new CalculatorApplication();
+		assertEquals(5, calc.add(2, 3));
+    	}
+
+    	@Test
+    	public void testSubtract() {
+		CalculatorApplication calc = new CalculatorApplication();
+		assertEquals(1, calc.subtract(3, 2));
+    	}
+
+}
